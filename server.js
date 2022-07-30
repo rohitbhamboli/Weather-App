bc025f14a08cdf4a3291d4f3905e172eebf89c00
Rohit <rs30328102@gmail.com>
2022-07-30 16:28:54 +0530

    NodeJs Weather App first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const http = require("http");
+var requests = require("requests");
+
+const homePage = fs.readFileSync("index.html", "utf-8");
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempVal%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempMin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempMax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempStatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      "https://api.openweathermap.org/data/2.5/weather?q=yamunanagar&units=metric&appid=5def153ef4bce6b9592146ce7ac68baf"
+    )
+      .on("data", (chunk) => {
+        const objData = JSON.parse(chunk);
+        const arrData = [objData];
+        const realTimeData = arrData
+          .map((val) => replaceVal(homePage, val))
+          .join("");
+        res.write(realTimeData);
+        // console.log(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+
+        res.end();
+      });
+  }
+});
+server.listen(8000, "127.0.0.1");
